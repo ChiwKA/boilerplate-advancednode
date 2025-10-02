daed640fd54020a0284b904dc49417adbf4515cf
ChiwKA <hoangquan96@gmail.com>
2025-10-02 17:06:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const passport = require('passport')
 const session = require('express-session')
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
@@ -67,7 +68,7 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`)
       if (err) return done(err);
       if (!user) return done(null, false);
-      if (password != user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
       return done(null, user)
     })
   }))
@@ -101,9 +102,10 @@ myDB(async client => {
       } else if (user) {
         res.redirect('/');
       } else {
+        const hash = bcrypt.hashSync(req.body.password, 12)
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         },
           (err, doc) => {
             if (err) {
